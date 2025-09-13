e4bd956d6b69eef5c285d304f04691e83c5587ca
Roman Heinrich <roman.heinrich@gmail.com>
2025-09-14 01:39:38 +0200

    init

---
+++ lib/elixir_proto.ex
@@ -0,0 +1,18 @@
+defmodule ElixirProto do
+  @moduledoc """
+  Documentation for `ElixirProto`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirProto.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_proto_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirProtoTest do
+  use ExUnit.Case
+  doctest ElixirProto
+
+  test "greets the world" do
+    assert ElixirProto.hello() == :world
+  end
+end
